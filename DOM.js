0b8f53084c67981cd69ea24508762587c8bfd6b6
Antecar <antoncarlstedt@hotmail.com>
2017-11-17 13:21:05 +0100

    massa skit

--- DOM.js
+++ DOM.js
@@ -2,4 +2,12 @@ $(function() {
     $("#openinfo").click(function(){
         $("#info").toggle();
     });
+    
+    $("li").dblclick(function(){
+        $(this).toggleClass("selected");
+    });
+    
+    $("#bgcolor").change(function(){
+        $("body").css("background-color", $(this).val());
+    });
 });

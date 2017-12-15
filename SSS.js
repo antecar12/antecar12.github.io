8c99aec44f5de7e27c09695ebc390926a8336946
Antecar <antoncarlstedt@hotmail.com>
2017-12-15 13:44:08 +0100

    Whew SSS

--- SSS.js
+++ SSS.js
@@ -4,21 +4,21 @@ $(function() {
         $("#valkommen").hide();
         $("div").css("box-shadow", "inset 0 0 0px #000000");
         $("#schyssttext").show();
-        $("div#schysst").css("box-shadow", "inset 0 0 12px #000000");
+        $("div#schysstnav").css("box-shadow", "inset 0 0 12px #000000");
     });
     $("#sakernav").click(function(){
         $("section").hide();
         $("#valkommen").hide();
         $("div").css("box-shadow", "inset 0 0 0px #000000");
         $("#sakertext").show();
-        $("div#saker").css("box-shadow", "inset 0 0 12px #000000");
+        $("div#sakernav").css("box-shadow", "inset 0 0 12px #000000");
     });
     $("#standardnav").click(function(){
         $("section").hide();
         $("#valkommen").hide();
         $("div").css("box-shadow", "inset 0 0 0px #000000");
         $("#standardtext").show();
-        $("div#standard").css("box-shadow", "inset 0 0 12px #000000");
+        $("div#standardnav").css("box-shadow", "inset 0 0 12px #000000");
     });
     
 });

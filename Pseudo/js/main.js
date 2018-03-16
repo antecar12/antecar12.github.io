758ea351c3a869b1b44113682f0edd183e93aaf8
Antecar <antoncarlstedt@hotmail.com>
2018-03-16 12:55:33 +0100

    Namnvändaren
    pseudo

--- Pseudo/js/main.js
+++
@@ -1,9 +0,0 @@
-var game=new Phaser.Game(800,600,Phaser.AUTO);
-
-game.state.add('GameStart', GameStart);
-game.state.add('GameState', GameState);
-game.state.add('GameOver', GameOver);
-
-game.state.start('GameStart');
-
-            
